09ffac198a22b5f273766083bb75c7a45447fb4a
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-13 18:14:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(say)
-  if say == say.uppercase
-    puts "NO, NOT SINCE 1938!"
-  elsif say == "I LOVE YOU GRANDMA!"
+  if say == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  elsif say == say.uppercase
+      puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
